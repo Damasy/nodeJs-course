04499475bd91f89098c690dc5dbb5d3e72040f1c
903601 <super.nova.egy@gmail.com>
2021-01-12 02:18:05 +0200

    fix util issues

--- 05-shop/util/database.js
+++ 05-shop/util/database.js
@@ -1,28 +1,45 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// >>>>>>>>>> this is ordinary way to use db directly without sequelize <<<<<<<<<<<<
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: "root",
+// });
+
+// module.exports = pool.promise();
+
+// >>>>>>> sequelize <<<<<<<<<< //
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize("node-complete", "root", "root", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
+
+// module.exports = sequelize;
+
+const mongodb = require("mongodb");
+const Mongoient = mongodb.MongoClient;
 
 let _db;
-
-const mongoConnect = callback => {
-  MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+const mongoConnect = (cb) => {
+  Mongoient.connect(
+    "mongodb+srv://root:root@node-complete.0uxmx.mongodb.net/shop?retryWrites=true&w=majority"
   )
-    .then(client => {
-      console.log('Connected!');
+    .then((client) => {
       _db = client.db();
-      callback();
+      cb();
     })
-    .catch(err => {
-      console.log(err);
-      throw err;
-    });
+    .catch((err) => console.log(err));
 };
 
 const getDb = () => {
   if (_db) {
     return _db;
   }
-  throw 'No database found!';
+  throw "No database found";
 };
 
 exports.mongoConnect = mongoConnect;

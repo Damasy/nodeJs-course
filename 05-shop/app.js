c1fa7b7b6c35dfb4b942a932725aefb448ec28bd
903601 <super.nova.egy@gmail.com>
2021-01-18 03:04:35 +0200

    working with cookies

--- 05-shop/app.js
+++ 05-shop/app.js
@@ -5,21 +5,42 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const mongoDBStore = require("connect-mongodb-session")(session);
+
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://root:root@node-complete.0uxmx.mongodb.net/shop";
 const app = express();
+const store = new mongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById("5ffcf71b05dfb400c44f3821")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
@@ -29,14 +50,12 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://root:root@node-complete.0uxmx.mongodb.net/shop?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     User.findOne().then((user) => {
       if (!user) {
--- 05-shop/controllers/admin.js
+++ 05-shop/controllers/admin.js
@@ -5,6 +5,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
@@ -43,6 +44,7 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
+        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
@@ -71,6 +73,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
+        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
